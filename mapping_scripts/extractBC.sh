3359e401572bc8447bdc247f7166bc46af73e0aa
Anna Alemany <annalemany@gmail.com>
2019-12-03 10:27:18 +0100

    Create extractBC.sh

---
+++ mapping_scripts/extractBC.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]
+then
+    echo "Please, give: "
+    echo "1) input root to fastq files"
+    echo "2) path to concatenator.py"
+    exit
+fi
+
+outfq=$1
+protocol=celseq2
+path2scripts=$3
+
+python3 ${path2scripts}/concatenator.py --fqf ${outfq} --cbcfile ${path2scripts}/bc_celseq2.tsv --cbchd 0 --lenumi 6 --umifirst
+gzip ${outfq}_cbc.fastq
+
